f94db804a431f956d1d682f082ed1db23847f5d7
Ruchir Toshniwal <ruchirtoshniwal450@gmail.com>
2019-10-31 22:39:45 +0530

    printing matrix and printing count variable
    now we can see the value of count variable

--- matrix addition.c
+++ matrix addition.c
@@ -62,6 +62,13 @@ void main()
             }
        count=count+1;
          printf("\n Matrix 3\n");
+	for(i=0;i<m;i++)
+            {
+                for(j=0;j<n;j++)
+printf(" %d ",c[i][j]);
+                printf("\n ");
+             }
+printf("Count = %d",count);
       
 
    
